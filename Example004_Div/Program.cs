626d5055171d93914e8e37cd0cd24a0f3b6b3c8a
koflex <koflex@mail.ru>
2022-06-05 15:54:01 +0300

    Add Example004 Div project

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberA = 12;
+int numberB = 5;
+Console.WriteLine(numberA / numberB);
+
+// int - контейнер целого числа
+// double - контейнер вещественого числа
+
+double numberC = 12;
+double numberD = 5;
+Console.WriteLine(numberC / numberD);
