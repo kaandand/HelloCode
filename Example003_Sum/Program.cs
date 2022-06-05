03f3b934b4086de1e53bd5ed97535f3dfdf19b80
koflex <koflex@mail.ru>
2022-06-05 15:34:06 +0300

    add example003 sum project

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,5 @@
+﻿int numberA = 31;
+int numberB = 15;
+// вводим промежуточное значение result, можно сразу записать ConsoleWriteLine(numberA + numberB)
+int result = numberA + numberB;
+Console.WriteLine(result);
